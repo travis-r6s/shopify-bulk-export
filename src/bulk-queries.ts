f2d901dfb98391a650f61c5d60131d361f23ad00
Travis Reynolds <travis@travisreynolds.dev>
2024-07-18 13:24:12 +0100

    Update to handle more errors, and add more logs

--- src/bulk-queries.ts
+++ src/bulk-queries.ts
@@ -1,5 +1,12 @@
+interface Error {
+  message: string
+  path?: string[]
+  extensions?: object
+}
+
 export interface GraphQLResponse<Query> {
   data: Query
+  errors?: Error[]
 }
 
 export const StartBulkQuery = `mutation StartBulkQuery ($query: String!) {
@@ -97,6 +104,15 @@ export const BulkStatusQuery = `query BulkStatus ($id: ID!) {
 export interface BulkStatusQueryType {
   __typename?: 'QueryRoot'
   bulk?: {
-    __typename: 'App'
-  } | { __typename: 'AppCredit' } | { __typename: 'AppInstallation' } | { __typename: 'AppPurchaseOneTime' } | { __typename: 'AppRevenueAttributionRecord' } | { __typename: 'AppSubscription' } | { __typename: 'AppUsageRecord' } | { __typename: 'BasicEvent' } | { __typename: 'BulkOperation', id: string, status: BulkOperationStatus, errorCode?: BulkOperationErrorCode | null, createdAt: any, completedAt?: any | null, objectCount: any, fileSize?: any | null, url?: any | null, partialDataUrl?: any | null } | { __typename: 'CalculatedOrder' } | { __typename: 'Channel' } | { __typename: 'ChannelDefinition' } | { __typename: 'ChannelInformation' } | { __typename: 'CheckoutProfile' } | { __typename: 'Collection' } | { __typename: 'CommentEvent' } | { __typename: 'Company' } | { __typename: 'CompanyAddress' } | { __typename: 'CompanyContact' } | { __typename: 'CompanyContactRole' } | { __typename: 'CompanyContactRoleAssignment' } | { __typename: 'CompanyLocation' } | { __typename: 'Customer' } | { __typename: 'CustomerPaymentMethod' } | { __typename: 'CustomerVisit' } | { __typename: 'DeliveryCarrierService' } | { __typename: 'DeliveryCondition' } | { __typename: 'DeliveryCountry' } | { __typename: 'DeliveryLocationGroup' } | { __typename: 'DeliveryMethod' } | { __typename: 'DeliveryMethodDefinition' } | { __typename: 'DeliveryParticipant' } | { __typename: 'DeliveryProfile' } | { __typename: 'DeliveryProfileItem' } | { __typename: 'DeliveryProvince' } | { __typename: 'DeliveryRateDefinition' } | { __typename: 'DeliveryZone' } | { __typename: 'DiscountAutomaticBxgy' } | { __typename: 'DiscountAutomaticNode' } | { __typename: 'DiscountCodeNode' } | { __typename: 'DiscountNode' } | { __typename: 'DiscountRedeemCodeBulkCreation' } | { __typename: 'Domain' } | { __typename: 'DraftOrder' } | { __typename: 'DraftOrderLineItem' } | { __typename: 'DraftOrderTag' } | { __typename: 'Duty' } | { __typename: 'ExternalVideo' } | { __typename: 'Fulfillment' } | { __typename: 'FulfillmentEvent' } | { __typename: 'FulfillmentLineItem' } | { __typename: 'FulfillmentOrder' } | { __typename: 'FulfillmentOrderDestination' } | { __typename: 'FulfillmentOrderLineItem' } | { __typename: 'FulfillmentOrderMerchantRequest' } | { __typename: 'GenericFile' } | { __typename: 'GiftCard' } | { __typename: 'InventoryItem' } | { __typename: 'InventoryLevel' } | { __typename: 'LineItem' } | { __typename: 'LineItemMutable' } | { __typename: 'Location' } | { __typename: 'MailingAddress' } | { __typename: 'Market' } | { __typename: 'MarketRegionCountry' } | { __typename: 'MarketWebPresence' } | { __typename: 'MarketingActivity' } | { __typename: 'MarketingEvent' } | { __typename: 'MediaImage' } | { __typename: 'Metafield' } | { __typename: 'MetafieldDefinition' } | { __typename: 'MetafieldStorefrontVisibility' } | { __typename: 'Model3d' } | { __typename: 'OnlineStoreArticle' } | { __typename: 'OnlineStoreBlog' } | { __typename: 'OnlineStorePage' } | { __typename: 'Order' } | { __typename: 'OrderDisputeSummary' } | { __typename: 'OrderTransaction' } | { __typename: 'PaymentMandate' } | { __typename: 'PaymentSchedule' } | { __typename: 'PaymentTerms' } | { __typename: 'PaymentTermsTemplate' } | { __typename: 'PriceList' } | { __typename: 'PriceRule' } | { __typename: 'PriceRuleDiscountCode' } | { __typename: 'PrivateMetafield' } | { __typename: 'Product' } | { __typename: 'ProductOption' } | { __typename: 'ProductTaxonomyNode' } | { __typename: 'ProductVariant' } | { __typename: 'Publication' } | { __typename: 'Refund' } | { __typename: 'SavedSearch' } | { __typename: 'ScriptTag' } | { __typename: 'Segment' } | { __typename: 'SellingPlan' } | { __typename: 'SellingPlanGroup' } | { __typename: 'Shop' } | { __typename: 'ShopPolicy' } | { __typename: 'ShopifyPaymentsAccount' } | { __typename: 'ShopifyPaymentsBankAccount' } | { __typename: 'ShopifyPaymentsDispute' } | { __typename: 'ShopifyPaymentsDisputeEvidence' } | { __typename: 'ShopifyPaymentsDisputeFileUpload' } | { __typename: 'ShopifyPaymentsDisputeFulfillment' } | { __typename: 'ShopifyPaymentsPayout' } | { __typename: 'ShopifyPaymentsVerification' } | { __typename: 'StaffMember' } | { __typename: 'StandardMetafieldDefinitionTemplate' } | { __typename: 'StorefrontAccessToken' } | { __typename: 'SubscriptionBillingAttempt' } | { __typename: 'SubscriptionContract' } | { __typename: 'SubscriptionDraft' } | { __typename: 'TenderTransaction' } | { __typename: 'TransactionFee' } | { __typename: 'UrlRedirect' } | { __typename: 'UrlRedirectImport' } | { __typename: 'Video' } | { __typename: 'WebPixel' } | { __typename: 'WebhookSubscription' } | null
+    __typename: 'BulkOperation'
+    id: string
+    status: BulkOperationStatus
+    errorCode?: BulkOperationErrorCode | null
+    createdAt: any
+    completedAt?: any | null
+    objectCount: any
+    fileSize?: any | null
+    url?: any | null
+    partialDataUrl?: any | null
+  } | null
 }
--- src/index.ts
+++ src/index.ts
@@ -82,36 +82,57 @@ export function replaceQueryVariables(query: string | TypedDocumentNode, variabl
 }
 
 async function startBulkQuery(query: string, client: Got, ctx: FunctionContext): Promise<string> {
-  const { data } = await client.post<GraphQLResponse<StartBulkQueryType>>('graphql.json', {
+  const { data, errors } = await client.post<GraphQLResponse<StartBulkQueryType>>('graphql.json', {
     json: { query: StartBulkQuery, variables: { query } },
     resolveBodyOnly: true,
     responseType: 'json',
   })
 
+  if (errors?.length) {
+    ctx.logger.error('Received errors during bulk status query:')
+    for (const error of errors) {
+      ctx.logger.error(error.message, error)
+    }
+
+    throw new Error(errors[0].message)
+  }
+
+  if (!data.bulkOperationRunQuery) {
+    ctx.logger.error('Missing `data.bulkOperationRunQuery` key in response from bulk operation start mutation.', data)
+    throw new Error('Missing `data.bulkOperationRunQuery` key in response from bulk operation start mutation.')
+  }
+
   if (data.bulkOperationRunQuery?.userErrors.length) {
+    ctx.logger.error('Received errors during bulk operation create mutation:')
+    for (const error of data.bulkOperationRunQuery?.userErrors) {
+      ctx.logger.error(error.message, error)
+    }
+
     throw new Error(data.bulkOperationRunQuery.userErrors[0].message)
   }
 
   if (data.bulkOperationRunQuery?.bulkOperation?.status === BulkOperationStatus.Failed) {
+    ctx.logger.error(`Bulk operation create mutation returned response: ${data.bulkOperationRunQuery.bulkOperation.status}`, data.bulkOperationRunQuery.bulkOperation)
     throw new Error('Failed to create bulk operation.')
   }
 
   if (!data.bulkOperationRunQuery?.bulkOperation?.id) {
-    throw new Error('Missing buk operation ID.')
+    ctx.logger.error(`Bulk operation create mutation is missing an ID from the response.`, data.bulkOperationRunQuery.bulkOperation)
+    throw new Error('Missing bulk operation ID from the returned response.')
   }
 
-  ctx.logger.debug(`Successfully created bulk query with ID: ${data.bulkOperationRunQuery.bulkOperation.id}`)
+  ctx.logger.debug(`Successfully created bulk query with ID: ${data.bulkOperationRunQuery.bulkOperation.id}`, data.bulkOperationRunQuery.bulkOperation)
 
   return data.bulkOperationRunQuery.bulkOperation.id
 }
 
-async function waitForQuery(bulkOperationId: string, client: Got, interval: number = 20000, ctx: FunctionContext): Promise<string> {
-  let downloadUrl = ''
+async function waitForQuery(bulkOperationId: string, client: Got, interval: number = 20000, ctx: FunctionContext): Promise<string | undefined> {
+  let downloadUrl: string | undefined
 
   await pWaitFor(async () => {
     ctx.logger.debug(`Checking bulk query status of operation ${bulkOperationId}`)
 
-    const { data } = await client.post<GraphQLResponse<BulkStatusQueryType>>('graphql.json', {
+    const { data, errors } = await client.post<GraphQLResponse<BulkStatusQueryType>>('graphql.json', {
       resolveBodyOnly: true,
       responseType: 'json',
       json: {
@@ -120,29 +141,55 @@ async function waitForQuery(bulkOperationId: string, client: Got, interval: numb
       },
     })
 
+    if (errors?.length) {
+      ctx.logger.error('Received errors during bulk status query:')
+      for (const error of errors) {
+        ctx.logger.error(error.message, error)
+      }
+
+      throw new Error(errors[0].message)
+    }
+
+    if (!data.bulk) {
+      ctx.logger.error('Missing `data.bulk` key in response from bulk operation status query.', data)
+      throw new Error('Missing `data.bulk` key in response from bulk operation status query.')
+    }
+
     if (data.bulk?.__typename !== 'BulkOperation') {
-      throw new Error('Wrong type!')
+      ctx.logger.error(`__typename returned from the bulk operation status query was ${data.bulk.__typename}, NOT the expected 'BulkOperation'.`, data.bulk)
+      throw new Error('Wrong typename returned from the bulk operation status query - this is likely a library error, so open an issue on GitHub.')
     }
 
     if (data.bulk.errorCode) {
-      throw new Error(data.bulk.errorCode)
+      ctx.logger.error(`Bulk operation failed, with an error code of ${data.bulk.errorCode}`, data.bulk)
+      throw new Error(`Bulk operation failed, with an error code of ${data.bulk.errorCode}`)
     }
 
     if (data.bulk.status === BulkOperationStatus.Completed) {
       if (Number(data.bulk.objectCount) === 0) {
-        throw new Error('No objects exist in this export.')
+        ctx.logger.debug(`The bulk operation completed, but no objects exist in this export (count of ${data.bulk.objectCount}).`, data.bulk)
+        ctx.logger.info('No objects exist in this export - check your input query if this was not expected.')
+
+        return true
       }
+
+      ctx.logger.debug(`Found status of ${BulkOperationStatus.Completed}:`, data.bulk)
 
       downloadUrl = data.bulk.url
       return true
     }
 
-    ctx.logger.debug(`Bulk query hasn't finished yet, waiting ${interval}ms`)
+    ctx.logger.debug(`Bulk query hasn't finished yet, waiting ${interval}ms.`, `Last status: ${data.bulk.status}`)
 
     return false
   }, { interval })
 
-  ctx.logger.debug(`Bulk query has finished - download URL: ${downloadUrl}`)
+  if (downloadUrl) {
+    ctx.logger.debug(`Bulk query has finished - download URL: ${downloadUrl}`)
+  }
+  else {
+    ctx.logger.debug('Finished `waitFor`, but `downloadUrl` is nullish, so returning')
+  }
 
   return downloadUrl
 }
@@ -152,20 +199,29 @@ export type BaseResult<T> = T & {
 }
 
 async function downloadData<T>(downloadUrl: string, ctx: FunctionContext): Promise<Array<BaseResult<T>>> {
-  const rl = createInterface(got.stream(downloadUrl))
+  try {
+    const rl = createInterface(got.stream(downloadUrl))
 
-  const nodes: Array<BaseResult<T>> = []
-  for await (const line of rl) {
+    const nodes: Array<BaseResult<T>> = []
+    for await (const line of rl) {
     // Shopify seems to add a newline at the bottom of the file now, so check for that
-    if (line) {
-      const data = JSON.parse(line)
-      nodes.push(data)
+      if (line) {
+        const data = JSON.parse(line)
+        nodes.push(data)
+      }
     }
+
+    ctx.logger.debug('Finished downloading and parsing JSONL file')
+
+    return nodes
   }
+  catch (err) {
+    const error = err as Error
+    ctx.logger.error(`Failed to download or parse JSONL data:`)
+    ctx.logger.error(error)
 
-  ctx.logger.debug('Finished downloading and parsing JSONL file')
-
-  return nodes
+    throw error
+  }
 }
 
 /**
@@ -230,6 +286,10 @@ async function run<T = unknown>(input: PluginInput): Promise<Array<BaseResult<T>
   logger.debug('Waiting for bulk query to finish')
 
   const bulkDownloadUrl = await waitForQuery(bulkOperationId, client, input.interval, ctx)
+  if (!bulkDownloadUrl) {
+    logger.debug(`\`bulkDownloadUrl\` was null (from \`waitForQuery\`), so returning empty array.`, { bulkDownloadUrl })
+    return []
+  }
 
   logger.debug('Downloading and parsing bulk query data')
 
