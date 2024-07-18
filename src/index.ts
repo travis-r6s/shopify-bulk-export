713db7f5311d969e5284add0c32ab0944ed5468e
Travis Reynolds <travis@travisreynolds.dev>
2024-07-18 15:30:05 +0100

    Export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './functions/bulk-export'
 export * from './functions/resume-export'
 
 export { runBulkExport as default } from './functions/bulk-export'
+
+export * from './types/results'
+export * from './types/input'
